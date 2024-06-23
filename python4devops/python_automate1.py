651f6b5a6deba645d0c3cbb21c9f85c16962aa92
PrincessUjay <onyekabaprincess@gmail.com>
2024-06-23 16:48:35 +0100

    commit changes

--- python4devops/python_automate1.py
+++ python4devops/python_automate1.py
@@ -1,4 +1,34 @@
 import os
 import sys
+import subprocess
 
-print(sys.version)
+
+
+command = ["vagrant", "ubuntu/focal64"]
+command2 = ["vagrant", "up"]
+
+dirPath = "\Users\onyek\Documents\KODECAMP4.0\python4devops"
+
+sp = subprocess.Popen(command,shell=False,stdout=subprocess,PIPE,stderr=subprocess,PIPE,universal_newlines=True)
+sp2 = subprocess.Popen(command,shell=False,stdout=subprocess,PIPE,stderr=subprocess,PIPE,universal_newlines=True)
+
+
+def get_version():
+    print("\nExecuting get_version() Function....\n")
+
+    version = sys.version
+
+    versionID = version[0:6]
+    return versionID
+
+
+    def create_folder():
+
+        print("\nExecuting create_folder() Function....\n")
+
+
+        vid = get_version()
+
+
+        if "3" in vid:
+            
